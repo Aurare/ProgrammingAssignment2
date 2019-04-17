6642a714bc4b6715c070c76ca5fded1060db14a7
Aurore Paligot <aurorepaligot@hotmail.com>
2019-04-17 23:51:04 +0200

    Update

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions working together to put inverted matrices on the cache
+#and reduce computational demand
 
-## Write a short comment describing this function
+## This function returns a list object and works with getters and setters
+#that make values availables in the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This function uses the list created in the first function, and either retrieves
+#a cached inverted matrix or creates one (with solve function) and puts it in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
